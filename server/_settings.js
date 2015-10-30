8dd07167008e1e79ace25504ff5f9288ea1d7875
Kevin James <KevinJames@thekev.in>
2015-10-30 16:29:52 -0400

    cleanup server stuff

--- server/_settings.js
+++ server/_settings.js
@@ -2,18 +2,21 @@
 //
 // To configure your own Twitter keys, see:
 //   https://github.com/meteor/meteor/wiki/Configuring-Twitter-in-Local-Market
-if (typeof Meteor.settings === 'undefined')
+if (typeof Meteor.settings === 'undefined') {
   Meteor.settings = {};
+}
 
 _.defaults(Meteor.settings, {
   twitter: {
-    consumerKey: "jFzDvBaf3rxZJxiXADuww4Ali", 
+    consumerKey: "jFzDvBaf3rxZJxiXADuww4Ali",
     secret: "GDPFW3fo3hwrUHu5aSyCQUMM5B1Izs1ViBqKlA0lRCnPpxzGm0"
   }
 });
 
 ServiceConfiguration.configurations.upsert(
-  { service: "twitter" },
+  {
+    service: "twitter"
+  },
   {
     $set: {
       consumerKey: Meteor.settings.twitter.consumerKey,
--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,3 @@
-Meteor.publish('bookmarkCounts', function() {
-  return BookmarkCounts.find();
-});
-
 Meteor.publish('latestActivity', function () {
   return Activities.latest();
 });
@@ -10,10 +6,7 @@ Meteor.publish('feed', function() {
   return Activities.find({}, {sort: {date: -1}, limit: 10});
 });
 
-Meteor.publish('recipe', function(name) {
+Meteor.publish('event', function(name) {
   check(name, String);
-  return [
-    BookmarkCounts.find({recipeName: name}),
-    Activities.find({recipeName: name})
-  ];
+  return Activities.find({eventName: name});
 });
