d9427b3e9ab6b3822666d922b5c00333fbcf4e35
Kevin James <KevinJames@thekev.in>
2015-10-30 17:19:39 -0400

    add sample data

--- client/templates/event-item.js
+++ client/templates/event-item.js
@@ -1,10 +1,5 @@
 Template.eventItem.helpers({
   path: function () {
     return Router.path('event', this.event);
-  },
-  highlightedClass: function () {
-    if (this.size === 'large') {
-      return 'highlighted';
-    }
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -5,8 +5,3 @@ Meteor.publish('latestActivity', function () {
 Meteor.publish('feed', function() {
   return Activities.find({}, {sort: {date: -1}, limit: 10});
 });
-
-Meteor.publish('event', function(name) {
-  check(name, String);
-  return Activities.find({eventName: name});
-});
