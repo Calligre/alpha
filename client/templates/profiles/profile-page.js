06dfed6cfe54c1e2da939fc5876ee75a473bb079
Kevin James <KevinJames@thekev.in>
2015-11-09 13:19:18 -0500

    basic profile page

--- client/templates/profiles/profile-page.js
+++ client/templates/profiles/profile-page.js
@@ -1,7 +1,7 @@
 Template.profilePage.helpers({
     data: function() {
         var matches = Meteor.users.find({_id: Router.current().options.params.id}).fetch();
-        if(matches.length != 1){
+        if (matches.length != 1) {
             console.error("Holy fuck how did you do that?")
         }
         return matches[0];
