5afd307566b99e58df590e4a3fd038173c32c833
Lara <numbersandlettersandallthat@gmail.com>
2015-11-09 22:46:21 -0500

    Adding UI for points value to profiles and user profile pages

--- server/users.js
+++ server/users.js
@@ -4,7 +4,8 @@ Accounts.onCreateUser(function(options, user) {
     school: 'The School of Life',
     year: '2015',
     program: 'Engineering',
-    about: 'I love CDE 2015!'
+    about: 'I love CDE 2015!',
+    points: 42
   });
 
   return user;
