0649f5c6078d31cfb4a82da476cde3235936a6c3
Kyle Lexmond <github@kyl191.net>
2015-11-11 14:40:01 -0500

    alternate sorting on people page

--- client/templates/profiles/profile-list.js
+++ client/templates/profiles/profile-list.js
@@ -1,8 +1,24 @@
 Template.profileList.helpers({
   profiles: function() {
-    return Meteor.users.find({}, {sort: {name: -1}});
+    if (Session.get("sort_by_points")){
+      return Meteor.users.find({}, {sort: {points: -1}});
+    } else {
+      return Meteor.users.find({}, {sort: {name: -1}});
+    }
   },
   isReady: function() {
     return Router.current().userSubscription.ready();
   }
 });
+
+Template.profileList.events({
+  'click .js-points': function() {
+    Session.set("sort_by_points", !Session.get("sort_by_points"));
+  }
+});
+
+if (Meteor.is_client){
+  Meteor.startup(function(){
+    Session.set("sort_by_points", false);
+  });
+}
