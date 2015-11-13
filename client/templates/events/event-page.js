147a4244b57d01af684e0b74830d223a62df2fea
StevenLocke <s3locke@uwaterloo.ca>
2015-11-12 20:58:36 -0500

    Currently working shit

--- client/templates/events/event-page.js
+++ client/templates/events/event-page.js
@@ -68,15 +68,13 @@ Template.eventPage.helpers({
       return "" + padLessThan10(now.getUTCFullYear()) + padLessThan10(parseInt(now.getUTCMonth()) + 1) + padLessThan10(now.getUTCDate()) + "T" + padLessThan10(now.getUTCHours()) + padLessThan10(now.getUTCMinutes()) + padLessThan10(now.getUTCSeconds()) + "Z";
     };
 
-    var cal = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//AT Content Types//AT Event//EN\nX-WR-TIMEZONE:America/Toronto\nMETHOD:PUBLISH\n";
+    var cal = "BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\nX-WR-TIMEZONE:America/Toronto\n";
     cal += "BEGIN:VEVENT\n";
     cal += "UID:" + attendingEvent['_id'] + "@cde.cfes.ca\n"
     cal += "DTSTAMP:" + dateToICSString(new Date()) + "\n";
-    cal += "CREATED:" + attendingEvent['title'] + "\n";
-    cal += "LAST-MODIFIED:" + attendingEvent['title'] + "\n";
-    cal += "SUMMARY:" + attendingEvent['title'] + "\n";
     cal += "DTSTART:" + dateToICSString(attendingEvent['startDate']) + "\n";
     cal += "DTEND:" + dateToICSString(attendingEvent['endDate']) + "\n";
+    cal += "SUMMARY:" + attendingEvent['title'] + "\n";
     cal += "DESCRIPTION:";
     if(attendingEvent['speaker']) {
       cal += "Speaker: " + attendingEvent['speaker'] + "; ";
