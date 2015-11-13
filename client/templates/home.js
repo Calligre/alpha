955f7e02221ac473dac4d4212730ae54e51ee7fd
Kevin James <KevinJames@thekev.in>
2015-11-13 09:51:48 -0500

    Revert "fix login bug"
    This reverts commit e6a083e9882ff6f0ce2d2b6f1079fa258def735a.

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,27 +1,23 @@
 Template.login.events({
   'click .sign-in-text-facebook': function(event) {
     Meteor.loginWithFacebook({}, function(err) {
-      if (err) {
-        alert(err);
-      } else {
-        var response = HTTP.get(
-          'https://graph.facebook.com/v2.5/' + Meteor.user().services.facebook.id + '/picture', {
-            params: {
-              redirect: false,
-              type: 'large'
-            }
-          }, function(err, data) {
-            Meteor.users.update({_id: Meteor.userId()},
-                                {$set: {'services.facebook.image_url': data.data.data.url}});
+      var response = HTTP.get(
+        'https://graph.facebook.com/v2.5/' + Meteor.user().services.facebook.id + '/picture', {
+          params: {
+            redirect: false,
+            type: 'large'
           }
-        );
-      }
+        }, function(err, data) {
+          Meteor.users.update({_id: Meteor.userId()},
+                              {$set: {'services.facebook.image_url': data.data.data.url}});
+        }
+      );
     });
+  },
+  'click .sign-in-text-twitter': function(event) {
+    Meteor.loginWithTwitter();
+  },
+  'click .sign-in-text-linkedin': function(event) {
+    Meteor.loginWithLinkedin();
   }
 });
-
-Template.login.helpers({
-  services: function() {
-    return [{name: 'twitter'}, {name: 'linkedin'}];
-  }
-})
