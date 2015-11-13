e71f63b3a047166a633d23cfaf9f1f0dcc8aceef
Kevin James <KevinJames@thekev.in>
2015-11-13 02:24:42 -0500

    fix facebook profile image

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,3 +1,23 @@
-Template.login.service = function() {
-  return [{name: 'facebook'}, {name: 'twitter'}, {name: 'linkedin'}];
-};
+Template.login.events({
+  'click .sign-in-text-facebook': function(event) {
+    Meteor.loginWithFacebook({}, function(err) {
+      var response = HTTP.get(
+        'https://graph.facebook.com/v2.5/' + Meteor.user().services.facebook.id + '/picture', {
+          params: {
+            redirect: false,
+            type: 'large'
+          }
+        }, function(err, data) {
+          Meteor.users.update({_id: Meteor.userId()},
+                              {$set: {'services.facebook.image_url': data.data.data.url}});
+        }
+      );
+    });
+  },
+  'click .sign-in-text-twitter': function(event) {
+    Meteor.loginWithTwitter();
+  },
+  'click .sign-in-text-linkedin': function(event) {
+    Meteor.loginWithLinkedin();
+  }
+});
