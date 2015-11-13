c589b850b7fb804c1b37f6f64e49906fb494a0af
Kevin James <KevinJames@thekev.in>
2015-11-13 09:54:00 -0500

    no simple signon

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,23 +1,35 @@
 Template.login.events({
   'click .sign-in-text-facebook': function(event) {
     Meteor.loginWithFacebook({}, function(err) {
-      var response = HTTP.get(
-        'https://graph.facebook.com/v2.5/' + Meteor.user().services.facebook.id + '/picture', {
-          params: {
-            redirect: false,
-            type: 'large'
+      if (err) {
+        alert(err.reason);
+      } else {
+        var response = HTTP.get(
+          'https://graph.facebook.com/v2.5/' + Meteor.user().services.facebook.id + '/picture', {
+            params: {
+              redirect: false,
+              type: 'large'
+            }
+          }, function(err, data) {
+            Meteor.users.update({_id: Meteor.userId()},
+                                {$set: {'services.facebook.image_url': data.data.data.url}});
           }
-        }, function(err, data) {
-          Meteor.users.update({_id: Meteor.userId()},
-                              {$set: {'services.facebook.image_url': data.data.data.url}});
-        }
-      );
+        );
+      }
     });
   },
   'click .sign-in-text-twitter': function(event) {
-    Meteor.loginWithTwitter();
+    Meteor.loginWithTwitter({}. function(err) {
+      if (err) {
+        alert(err.reason);
+      }
+    });
   },
   'click .sign-in-text-linkedin': function(event) {
-    Meteor.loginWithLinkedin();
+    Meteor.loginWithLinkedin({}. function(err) {
+      if (err) {
+        alert(err.reason);
+      }
+    });
   }
 });
