df9f1444c27e7ca0bd0a2e93ae5b3910f6139a8f
Kevin James <KevinJames@thekev.in>
2015-11-12 22:24:02 -0500

    hotfix: multiple users share profile image

--- client/templates/profiles/profile-item.js
+++ client/templates/profiles/profile-item.js
@@ -1,9 +1,9 @@
 Template.profileItem.helpers({
   avatar: function() {
-    if ('twitter' in Meteor.user().services) {
-      return Meteor.user().services.twitter.profile_image_url_https;
-    } else if ('facebook' in Meteor.user().services) {
-      return Meteor.user().services.facebook.image_url;
+    if ('twitter' in this.services) {
+      return this.services.twitter.profile_image_url_https;
+    } else if ('facebook' in this.services) {
+      return this.services.facebook.image_url;
     } else {
       return 'http://publications.uew.edu.gh/2015/sites/default/files/default_profile_pic.jpg';
     }
