4809522dcc9e14e3f77ab949210425357798e739
Clarisse Schneider <cmschnei@uwaterloo.ca>
2015-12-08 15:44:55 -0500

    extract CDE-specific variables and set in js

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,7 +1,19 @@
+Template.home.onRendered(function() {
+  var self = this;
+
+});
+
 Template.home.helpers({
   isReady: function() {
     return Accounts.loginServicesConfigured();
-  }
+  },
+  logo: 'img/app/logo.png',
+  conferenceShortName: 'CDE 2015',
+  conferenceName: 'Conference on Diversity in Engineering 2015',
+  facebook: 'https://www.facebook.com/conferenceondiversityinengineering',
+  twitter: 'https://twitter.com/diversityineng',
+  youtube: 'https://www.youtube.com/channel/UCy80Ydr1QB2dOA1D9Us2_7Q',
+  generalInformation: "Welcome to the Conference on Diversity in Engineering!\nThe University of Waterloo is proud to host the inaugural Conference on Diversity in Engineering this November 2015. As new initiative of the Canadian Federation of Engineering Students, CDE aims to equip tomorrow’s leaders with the knowledge and inspiration to positively impact the Canadian engineering industry. 150 of the brightest engineering students from across the country will join together to discuss and learn about the challenges and opportunities that are faced in building a representative engineering community."
 });
 
 Template.login.events({
